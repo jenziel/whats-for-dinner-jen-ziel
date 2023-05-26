0000b96c919c95a390878170d826824d57a95f17
Jen Ziel <jenziel400@gmail.com>
2023-05-26 17:39:48 -0400

    Add placeholder responses for the lets cook button function.  add event handlers.  create variables for the query selectors.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+var selectSide = document.querySelector("#Side")
+var selectMainDish = document.querySelector("#MainDish")
+var selectDessert = document.querySelector("#Dessert")
 var letsCookButton = document.querySelector("#lets-cook-button")
+
 
 //event listeners here:
 letsCookButton.addEventListener("click", generateRandomDish)
 
 var recipeBook = {
     sides: ["Fresh Summer Rolls", "Pickles", "Toast", "Tater Tots", "Cucumber Salad", "Breakfast Potatoes", "Rice",
-    "Crispy Tofu", "Roasted Root Vegetables", "Roasted Mushrooms", "Hush Puppies", "Slaw"],
-    mainDishes: ["Falafel Sandwich", "Bahn Mi", "Chicken Fried Rice", "Butternut Squash Soup",
-    "Ramen", "Spicy Chicken Sandwich", "Empanadas", "Pepperoni Pizza", "Chicken Soup", "Pesto Pasta", "Thai Yellow Curry",
-     "Hamburger", "Pollo a la Brasa", "Shrimp Tempura Sushi", "Meatballs"],
-     desserts:  ["Rice Pudding", "Bread Pudding", "Churros", "Flan", "Creme Brulee", "Funfetti Cake", "Brownies", "Pumpkin Pie",
-     "Macaroons", "Blueberry Cobbler", "Apple Pie", "Baklava", "Banana Bread", "Sugar Cookies"],
+            "Crispy Tofu", "Roasted Root Vegetables", "Roasted Mushrooms", "Hush Puppies", "Slaw"],
+    mainDishes: ["Falafel Sandwich", "Bahn Mi", "Chicken Fried Rice", "Butternut Squash Soup","Ramen",
+                 "Spicy Chicken Sandwich", "Empanadas", "Pepperoni Pizza", "Chicken Soup", "Pesto Pasta", "Thai Yellow Curry",
+                "Hamburger", "Pollo a la Brasa", "Shrimp Tempura Sushi", "Meatballs"],
+    desserts:  ["Rice Pudding", "Bread Pudding", "Churros", "Flan", "Creme Brulee", "Funfetti Cake", "Brownies", "Pumpkin Pie",
+                "Macaroons", "Blueberry Cobbler", "Apple Pie", "Baklava", "Banana Bread", "Sugar Cookies"],
 }
 
 
