0fdd46e2e2075bc92ca84f7b5d62a0e50fbcac95
Jen Ziel <jenziel400@gmail.com>
2023-05-26 17:16:21 -0400

    Add 3 arrays containing sides, main dishes, and desserts

---
+++ main.js
@@ -0,0 +1,29 @@
+var letsCookButton = document.querySelector("#lets-cook-button")
+
+var sides = ["Fresh Summer Rolls", "Pickles", "Toast", "Tater Tots", "Cucumber Salad", "Breakfast Potatoes", "Rice",
+ "Crispy Tofu", "Roasted Root Vegetables", "Roasted Mushrooms", "Hush Puppies", "Slaw"]
+
+var mainDishes = ["Falafel Sandwich", "Bahn Mi", "Chicken Fried Rice", "Butternut Squash Soup",
+ "Ramen", "Spicy Chicken Sandwich", "Empanadas", "Pepperoni Pizza", "Chicken Soup", "Pesto Pasta", "Thai Yellow Curry",
+  "Hamburger", "Pollo a la Brasa", "Shrimp Tempura Sushi", "Meatballs"]
+
+var desserts = ["Rice Pudding", "Bread Pudding", "Churros", "Flan", "Creme Brulee", "Funfetti Cake", "Brownies", "Pumpkin Pie",
+ "Macaroons", "Blueberry Cobbler", "Apple Pie", "Baklava", "Banana Bread", "Sugar Cookies"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
